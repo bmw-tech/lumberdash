259f7ac9c60300a8c4c444e5c117a036c0f391be
Jorge Coca <jorge.coca@bmwna.com>
2019-03-07 07:35:14 -0600

    Remove colorize as dependency

---
+++ plugins/colorize_lumberdash/lib/src/colorize_lumberdash.dart
@@ -0,0 +1,39 @@
+import 'colors/colorize.dart';
+import 'package:lumberdash/lumberdash.dart';
+
+/// [LumberdashClient] that colors your logs in the stdout depending
+/// on their severity level
+class ColorizeLumberdash extends LumberdashClient {
+
+  /// Prints a regular message to stdout without any color treatment
+  @override
+  void logMessage(String message, [Map<String, dynamic> extras]) {
+    print('Message { message: $message, extras: $extras }'); // no color added
+  }
+
+  /// Prints the given message in yellow
+  @override
+  void logWarning(String message, [Map<String, dynamic> extras]) {
+    final warning = Colorize('Warning { message: $message, extras: $extras }');
+    warning.yellow();
+    print(warning);
+  }
+
+  /// Prints the given message in red
+  @override
+  void logFatal(String message, [Map<String, dynamic> extras]) {
+    final fatal = Colorize('Fatal { message: $message, extras: $extras }');
+    fatal.red();
+    print(fatal);
+  }
+
+  /// Printst the given message in a red background with white 
+  /// characters
+  @override
+  void logError(dynamic exception, [dynamic stacktrace]) {
+    final error = Colorize('Error { exception: $exception, stacktrace: $stacktrace }');
+    error.bgRed();
+    error.white();
+    print(error);
+  }
+}
