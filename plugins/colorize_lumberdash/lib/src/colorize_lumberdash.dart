df8100fc89271df18026255f3b006f757af5dace
ericktorres <ericktorresdeveloper@gmail.com>
2019-10-10 13:15:34 -0500

    removed changes to colorized

--- plugins/colorize_lumberdash/lib/src/colorize_lumberdash.dart
+++ plugins/colorize_lumberdash/lib/src/colorize_lumberdash.dart
@@ -6,31 +6,31 @@ import 'package:lumberdash/lumberdash.dart';
 class ColorizeLumberdash extends LumberdashClient {
   /// Prints a regular message to stdout without any color treatment
   @override
-  void logMessage(String message, [Map<String, dynamic> extras, String eventName]) {
-    print('Message { message: $message, extras: $extras, eventName: $eventName }'); // no color added
+  void logMessage(String message, [Map<String, dynamic> extras]) {
+    print('Message { message: $message, extras: $extras }'); // no color added
   }
 
   /// Prints the given message in yellow
   @override
-  void logWarning(String message, [Map<String, dynamic> extras, String eventName]) {
+  void logWarning(String message, [Map<String, dynamic> extras]) {
     final warning =
-        yellow.wrap('Warning { message: $message, extras: $extras, eventName: $eventName }');
+        yellow.wrap('Warning { message: $message, extras: $extras }');
     print(warning);
   }
 
   /// Prints the given message in red
   @override
-  void logFatal(String message, [Map<String, dynamic> extras, String eventName]) {
-    final fatal = red.wrap('Fatal { message: $message, extras: $extras, eventName: $eventName }');
+  void logFatal(String message, [Map<String, dynamic> extras]) {
+    final fatal = red.wrap('Fatal { message: $message, extras: $extras }');
     print(fatal);
   }
 
   /// Printst the given message in a red background with white
   /// characters
   @override
-  void logError(dynamic exception, [dynamic stacktrace, String eventName]) {
+  void logError(dynamic exception, [dynamic stacktrace]) {
     final error =
-        white.wrap('Error { exception: $exception, stacktrace: $stacktrace, eventName: $eventName }');
+        white.wrap('Error { exception: $exception, stacktrace: $stacktrace }');
     final errorBg = backgroundRed.wrap(error);
     print(errorBg);
   }
