c2b336bf1135636a36a5c2be826f1356da3db04c
Jorge Coca <jorge.coca@bmwna.com>
2019-10-17 15:00:41 -0500

    Allow lumberdash to accept a list of clients

--- lib/src/lumberdash.dart
+++ lib/src/lumberdash.dart
@@ -2,46 +2,43 @@ import 'package:meta/meta.dart';
 
 import 'client/client.dart';
 
-/// Initialize the `lumberdashClient` internally
-LumberdashClient _lumberdashClient = SimpleClient();
-
-/// Get the instance of the given [LumberdashClient]
-LumberdashClient get lumberdashClient => _lumberdashClient;
+/// Initialize the `lumberdashClients` internally
+List<LumberdashClient> _lumberdashClients = [];
 
 /// As it name says, it puts Lumberdash to work by setting up its
-/// [LumberdashClient]. With this library you could use [SimpleClient]
-/// to start logging on stoudt, but the full power of Lumberdash can
-/// be achieved when you use a custom [LumberdashClient] that fits
-/// your logging requirements.
-putLumberdashToWork({@required LumberdashClient withClient}) {
-  assert(withClient != null);
-  _lumberdashClient = withClient;
+/// [LumberdashClient]. The full power of Lumberdash can
+/// be achieved when you use a custom [LumberdashClient], or multiple
+/// clients, that can fit your logging requirements.
+putLumberdashToWork({@required List<LumberdashClient> withClients}) {
+  assert(withClients != null);
+  _lumberdashClients = withClients;
 }
 
-/// It calls the `logMessage` method of the given [LumberdashClient]
+/// It calls the `logMessage` method of the each [LumberdashClient]
 /// passed to `putLumberdashToWork`, and returns the given `message`.
 String logMessage(String message, [Map<String, dynamic> extras]) {
-  _lumberdashClient.logMessage(message, extras);
+  // _lumberdashClient.logMessage(message, extras);
+  _lumberdashClients.forEach((c) => c.logMessage(message, extras));
   return message;
 }
 
-/// It calls the `logWarning` method of the given [LumberdashClient]
+/// It calls the `logWarning` method of the each [LumberdashClient]
 /// passed to `putLumberdashToWork`, and returns the given `message`.
 String logWarning(String message, [Map<String, dynamic> extras]) {
-  _lumberdashClient.logWarning(message, extras);
+  _lumberdashClients.forEach((c) => c.logWarning(message, extras));
   return message;
 }
 
-/// It calls the `logFatal` method of the given [LumberdashClient]
+/// It calls the `logFatal` method of the each [LumberdashClient]
 /// passed to `putLumberdashToWork`, and returns the given `message`.
 String logFatal(String message, [Map<String, dynamic> extras]) {
-  _lumberdashClient.logFatal(message, extras);
+  _lumberdashClients.forEach((c) => c.logFatal(message, extras));
   return message;
 }
 
-/// It calls the `logError` method of the given [LumberdashClient]
+/// It calls the `logError` method of the each [LumberdashClient]
 /// passed to `putLumberdashToWork`, and returns the given `exception`.
 dynamic logError(dynamic exception, [dynamic stacktrace]) {
-  _lumberdashClient.logError(exception, stacktrace);
+  _lumberdashClients.forEach((c) => c.logError(exception, stacktrace));
   return exception;
 }
