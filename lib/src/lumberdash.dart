47c494a39b88e9f9f13937f667dce2e134e30420
Jorge Coca <jorge.coca@bmwna.com>
2019-10-21 09:57:44 -0500

    Added ability to filter out clients when logging

--- lib/src/lumberdash.dart
+++ lib/src/lumberdash.dart
@@ -16,28 +16,67 @@ putLumberdashToWork({@required List<LumberdashClient> withClients}) {
 
 /// It calls the `logMessage` method of the each [LumberdashClient]
 /// passed to `putLumberdashToWork`, and returns the given `message`.
-String logMessage(String message, [Map<String, dynamic> extras]) {
-  _lumberdashClients.forEach((c) => c.logMessage(message, extras));
+/// If you want to avoid logging to a particular client that was
+/// previously registered in `putLumberdashToWork#withClients`, use
+/// `exceptFor` to filter out that client.
+String logMessage(
+  String message, {
+  Map<String, dynamic> extras,
+  List<Type> exceptFor: const [],
+}) {
+  _filterOutClientsAndLog(exceptFor, (c) => c.logMessage(message, extras));
   return message;
 }
 
 /// It calls the `logWarning` method of the each [LumberdashClient]
 /// passed to `putLumberdashToWork`, and returns the given `message`.
-String logWarning(String message, [Map<String, dynamic> extras]) {
-  _lumberdashClients.forEach((c) => c.logWarning(message, extras));
+/// If you want to avoid logging to a particular client that was
+/// previously registered in `putLumberdashToWork#withClients`, use
+/// `exceptFor` to filter out that client.
+String logWarning(
+  String message, {
+  Map<String, dynamic> extras,
+  List<Type> exceptFor: const [],
+}) {
+  _filterOutClientsAndLog(exceptFor, (c) => c.logWarning(message, extras));
   return message;
 }
 
 /// It calls the `logFatal` method of the each [LumberdashClient]
 /// passed to `putLumberdashToWork`, and returns the given `message`.
-String logFatal(String message, [Map<String, dynamic> extras]) {
-  _lumberdashClients.forEach((c) => c.logFatal(message, extras));
+/// If you want to avoid logging to a particular client that was
+/// previously registered in `putLumberdashToWork#withClients`, use
+/// `exceptFor` to filter out that client.
+String logFatal(
+  String message, {
+  Map<String, dynamic> extras,
+  List<Type> exceptFor: const [],
+}) {
+  _filterOutClientsAndLog(exceptFor, (c) => c.logFatal(message, extras));
   return message;
 }
 
 /// It calls the `logError` method of the each [LumberdashClient]
 /// passed to `putLumberdashToWork`, and returns the given `exception`.
-dynamic logError(dynamic exception, [dynamic stacktrace]) {
-  _lumberdashClients.forEach((c) => c.logError(exception, stacktrace));
+/// If you want to avoid logging to a particular client that was
+/// previously registered in `putLumberdashToWork#withClients`, use
+/// `exceptFor` to filter out that client.
+dynamic logError(
+  dynamic exception, {
+  dynamic stacktrace,
+  List<Type> exceptFor: const [],
+}) {
+  _filterOutClientsAndLog(exceptFor, (c) => c.logError(exception, stacktrace));
   return exception;
+}
+
+typedef Logger = Function(LumberdashClient client);
+
+void _filterOutClientsAndLog(
+  List<Type> clientsToFilterOut,
+  Logger logger,
+) {
+  _lumberdashClients
+      .where((client) => !clientsToFilterOut.contains(client.runtimeType))
+      .forEach((c) => logger(c));
 }
